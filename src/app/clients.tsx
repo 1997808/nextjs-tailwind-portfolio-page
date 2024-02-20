bc91f200aa1fad44463568220c4fef6d65a30ac5
Le Duy Khanh <khanhld13@vpbank.com.vn>
2024-02-20 18:22:55 +0700

    fix logo

--- src/app/clients.tsx
+++ src/app/clients.tsx
@@ -4,12 +4,11 @@ import Image from "next/image";
 import { Typography } from "@material-tailwind/react";
 
 const CLIENTS = [
-  "coinbase",
-  "spotify",
-  "pinterest",
-  "google",
-  "amazon",
-  "netflix",
+  "ncc.png",
+  "adamo.png",
+  "mirabo.svg",
+  "lynkid.svg",
+  "vpbank.svg",
 ];
 
 export function Clients() {
@@ -17,17 +16,17 @@ export function Clients() {
     <section className="px-8 py-28">
       <div className="container mx-auto text-center">
         <Typography placeholder={""} variant="h6" color="blue-gray" className="mb-8">
-          My awesome clients
+          Work Experience
         </Typography>
-        <div className="flex flex-wrap items-center justify-center gap-6">
+        <div className="flex flex-wrap items-center justify-center gap-8">
           {CLIENTS.map((logo, key) => (
             <Image
               key={key}
               alt={logo}
-              width={768}
-              height={768}
-              className="w-40"
-              src={`/logos/logo-${logo}.svg`}
+              width={200}
+              height={200}
+              className="w-48 max-h-16 object-scale-down"
+              src={`/works/logo-${logo}`}
             />
           ))}
         </div>
